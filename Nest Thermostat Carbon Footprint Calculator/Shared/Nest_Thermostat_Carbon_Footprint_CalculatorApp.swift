febab97a38ba80e1c1f99d29786c9712da9c588b
Reid Fleishman <reidfleishman5@gmail.com>
2022-03-25 19:01:22 -0400

    initial commit

---
+++ Nest Thermostat Carbon Footprint Calculator/Shared/Nest_Thermostat_Carbon_Footprint_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nest_Thermostat_Carbon_Footprint_CalculatorApp.swift
+//  Shared
+//
+//  Created by Reid Fleishman on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct Nest_Thermostat_Carbon_Footprint_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
